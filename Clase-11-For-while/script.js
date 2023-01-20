fd1f5a86ec8d12f3df7fcbf4b19c4bed3620c153
arielrolla <rolla.ariel@gmail.com>
2023-01-19 22:22:26 -0300

    Subida clase 11

---
+++ Clase-11-For-while/script.js
@@ -0,0 +1,50 @@
+/* for (let i = 1; i <= 5; i = i +1) {
+    console.log("hola" + i)
+} */
+
+
+/* for (let i = 1; i <= 3; i = i + 1){
+    let nombre = prompt('Ingrese nombre')
+    console.log("Hola "+ nombre)
+}
+
+for (let i = 11; i <= 55; i = i + 11){
+    console.log(i)
+}
+
+
+let valorInicial = prompt('Ingrese valor inicial')
+let valorFinal = prompt('Ingrese valor Final')
+
+for (let i = Number(valorInicial); i <= Number(valorFinal); i = i + 1){
+    console.log(i)
+} */
+
+/* let nombre = prompt("Ingrese nombre")
+while (nombre != "voldemort"){
+    alert("su nombre es "+nombre)
+    nombre = prompt("Ingrese nombre")
+}
+
+let numero = prompt("Ingrese un dato numerico")
+while (!isNaN(numero)){
+    alert("su numero es " + numero)
+    numero = prompt("Ingrese un dato numerico")
+}
+ */
+
+ let color = prompt('Ingrese un color')
+while(color != "rojo"){
+    alert("Su color es "+ color)
+    color = prompt('Ingrese un color')
+} 
+
+let acumulador = 0
+for(let i=1; i<=5; i = i+1){
+    console.log(acumulador=acumulador+i)
+}
+
+/* for/while
+break  rompe el loop, para ahi
+continue  pasa de largo ese bucle
+*/
