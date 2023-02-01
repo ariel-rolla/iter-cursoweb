d5acaa1b8e9f154af237ac0e0b4834463191f748
arielrolla <rolla.ariel@gmail.com>
2023-02-01 14:21:13 -0300

    dfgd

--- Clase-14-Obj-Metodos/script.js
+++ Clase-14-Obj-Metodos/script.js
@@ -4,17 +4,32 @@ const productos = [
     {
         title: "moto g8",
         price: 9000,
-        id: 1
+        id: 1,
+        marca: "motorola"
     },
     {
         title: "IPHONE 10",
         price: 3000,
-        id: 2
+        id: 2,
+        marca: "apple"
     },
     {
         title: "samsung s20+",
         price: 2000000,
-        id: 3
+        id: 3,
+        marca: "samsung"
+    },
+    {
+        title: "moto g9",
+        price: 10000,
+        id: 4,
+        marca: "motorola"
+    },
+    {
+        title: "moto g10",
+        price: 19000,
+        id: 5,
+        marca: "motorola"
     }
 ]
 
@@ -29,5 +44,20 @@ for(const producto of productos){
 
 
 //como se usa hoy
-/* productos.forEach((producto) => console.log (producto.title)) */
+
+//forEach recorre todo el array
+productos.forEach((producto) => console.log ("el celula es: "+producto.title)) 
+
+//filter para filtrar segun criterio
+
+const resultados = productos.filter((producto) =>  producto.marca == "motorola" && producto.price < 15000 )
+
+if (resultados.length < 1){
+    console.log ("No hay resultados de tu busqueda")
+}else {
+    console.log(resultados)
+}
+
+//find
+
 
