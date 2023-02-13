57d4a3ef16d58fc4fcb296b2e83d2e6db7243313
“ariel-rolla” <rolla.ariel@gmail.com>
2023-02-13 15:36:51 -0300

    drgdrgy

--- Clase-17/script.js
+++ Clase-17/script.js
@@ -3,15 +3,21 @@
 //const contenedor = document.getElementById("contenedor")
 
 const btn = document.getElementById("btn-click")
+const navegador = document.getElementById("navegador")
 
-btn.addEventListener("mouseover", () => {
+/* btn.addEventListener("mouseover", () => {
     btn.innerText="Haceme click"
 })
 
 btn.addEventListener("mouseleave", () => {
     btn.innerText="Boton"
-})
+}) */
 
 btn.addEventListener("click", () => {
-    alert("Hizo un click")
+    // alert("Hizo un click")
+
+    //Alterna de clase, si esta la saca, si no esta la crea en el elemento
+    navegador.classList.toggle("oculto")
+    /* btn.classList.toggle("rojo") */
 })
+
