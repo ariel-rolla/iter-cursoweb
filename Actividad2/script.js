4eeb6fbb5717d3f3d85cf1af09957e7460e9bf7b
arielrolla <rolla.ariel@gmail.com>
2023-04-24 14:49:26 -0300

    adsgfadsgfd

---
+++ Actividad2/script.js
@@ -0,0 +1,6 @@
+
+const carta = document.getElementById("card");
+carta.addEventListener("mouseover", () => {
+    carta.classList.add('border-primary');
+    carta.classList.add('text-primary');
+})
