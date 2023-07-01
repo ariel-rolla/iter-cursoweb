8e1b0545f295b4908b58e84a1b39f90391d4bb6c
arielrolla <rolla.ariel@gmail.com>
2023-07-01 05:46:47 -0300

    Modificada clase 24

--- Clase-24-2-NodeJsServer/index.js
+++ Clase-24-2-NodeJsServer/index.js
@@ -1,8 +1,39 @@
-//inicia el server para empezar a escuchar peticiones
+//ejecutar con comando: NODE index.js
+
 const express = require('express');
 
 const app = express(); 
 
-app.listen(8000);
+app.get('/productos', (req, res) => {
 
-//ejecutar con NODE index.js
+    const productos = [
+        {id:1, nombre:'Mouse', precio: 15600},
+        {id:2, nombre:'Teclado', precio: 35600},
+        {id:3, nombre:'Monitor', precio: 115600}
+    ];
+    res.json(productos);
+});
+
+app.get('/clientes', (req, res) => {
+
+    const productos = [
+        {id:10, nombre:'Jorge', apellido: 'Tordos'},
+        {id:14, nombre:'carlitos', apellido: 'Guirdos'}
+    ];
+    res.json(productos);
+});
+
+app.post('/productos', (req, res) => {
+    res.json({message: 'El producto se agrego correctamente'});
+});
+
+app.put('/productos/:id', (req, res) => {
+    res.json({message: 'El producto se modifico correctamente'});
+});
+
+app.delete('/productos', (req, res) => {
+    res.json({message: 'El producto se elimino  correctamente'});
+});
+
+app.listen(8000);  //inicia el server para empezar a escuchar peticiones
+
