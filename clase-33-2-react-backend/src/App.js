56ab2e5b15849259071f1eb298327e9a615f3255
arielrolla <rolla.ariel@gmail.com>
2023-07-27 14:27:36 -0300

    fghfh

--- clase-33-2-react-backend/src/App.js
+++ clase-33-2-react-backend/src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+//npx create-react-app clase-31-2-React para instalar el proyecto
+import Encabezado from "./components/Encabezado";
+import ListadoDeProductos from "./components/ListadoDeProductos";
+import Footer from "./components/Footer";
+import Button from "./components/Button";
+import {useState, useEffect} from "react"; //hooks 
 
 function App() {
+  //let nombre = "Elon";
+  const [nombre, setNombre] = useState("Elon") //useState - Cambiar estado de variable en vivo
+  const [cantidad, setCantidad] = useState(0);
+
+  const cambiarNombre = () => {
+    //nombre = "pepe";
+    setNombre("pepe");
+  }
+
+  function actualizarCantidad(){
+    setCantidad(cantidad + 1);
+  }
+
+  function mostrarMensajeDeBienvenida(){
+    console.log("Bienvenido")
+  }
+
+  useEffect(actualizarCantidad, [nombre]); // detecta un efecto debido a un cambio de estado de variable
+
+  useEffect(mostrarMensajeDeBienvenida, []);
+
+  // <> Framgent
+  // se puede pasar variables como props hacia componentes
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <> 
+      Cantidad de veces que cambio el nombre: {cantidad}
+      <div>{nombre}</div>
+      <button onClick={cambiarNombre}>Cambiar nombre</button>
+      <Button variant="danger" />
+
+      <Encabezado nombreDeLaEmpresa="Empresa T" nombreDueño={nombre} domicilio="xxx 222" />
+      <ListadoDeProductos />
+      <Footer />
+    </>
   );
 }
 
---
+++ clase-33-2-react-backend/src/components/Button.js
@@ -0,0 +1,5 @@
+function Button({variant}) {
+    return <button className={`btn btn-${variant}`} >Aceptar</button>
+}
+
+export default Button;
