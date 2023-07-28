3ade49f3dabdcc97693eef3f32e2cd0e59f653c2
arielrolla <rolla.ariel@gmail.com>
2023-07-28 14:40:20 -0300

    sdfsdf

--- clase-33-2-react-backend/src/App.js
+++ clase-33-2-react-backend/src/App.js
@@ -1,14 +1,18 @@
-//rfc plantilla de react
+//rfc - plantilla de react
+//npm install react-router-dom-v5.2.0 - Para navegacion de paginas
 
 import BarraDeNavegacion from "./components/BarraDeNavegacion";
+import ProductosPage from "./pages/ProductosPage";
+import ClientesPage from "./pages/ClientesPage";
 
 function App() {
 
   return (
     <> 
       <BarraDeNavegacion />
+      <ClientesPage />
+      <ProductosPage />
     </>
   );
 }
 
-export default App;
