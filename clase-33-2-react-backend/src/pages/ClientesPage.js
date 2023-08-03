99236fecd5e4cd4618304e99b8ac7972a16d6e68
arielrolla <rolla.ariel@gmail.com>
2023-08-03 14:47:13 -0300

    fghfhgfh

--- clase-33-2-react-backend/src/pages/ClientesPage.js
+++ clase-33-2-react-backend/src/pages/ClientesPage.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react'
-import { Button, Modal } from 'react-bootstrap';
+import { Button, Form, Modal } from 'react-bootstrap';
 
 export default function ClientesPage() {
 
   const [clientes, setClientes] = useState([]);
+
+  const [ showModalClientes, setshowModalClientes] = useState(false);
 
   //const [showModalCluente, set]
 
@@ -44,29 +46,18 @@ export default function ClientesPage() {
     return rows;
   }
 
-  const handleClose = () => {
-    console.log("cerrar el modal")
+  const handleShowModalClientes = () => {
+    setshowModalClientes(true);
+  }
+
+  const handleCloseModalClientes = () => {
+    setshowModalClientes(false);
   }
 
   return ( 
     <>
       <h1>Clientes</h1>
-      <Button>Nuevo</Button>
-
-      <Modal show={true} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
+      <Button className='mb-3' onClick={handleShowModalClientes}>Nuevo</Button>
 
       <table className='table'>
         <thead>
@@ -77,6 +68,52 @@ export default function ClientesPage() {
           {getTableBody()}
         </tbody>
       </table>
+
+      <Modal show={showModalClientes} onHide={handleCloseModalClientes}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+
+            <Form.Group className="mb-3">
+              <Form.Label>Nombre</Form.Label>
+              <Form.Control type="text" placeholder="Ingrese nombre" />
+            </Form.Group>
+            
+            <Form.Group className="mb-3">
+              <Form.Label>Apellido</Form.Label>
+              <Form.Control type="text" placeholder="Ingrese apellido" />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label>Calle</Form.Label>
+              <Form.Control type="text" placeholder="Ingrese calle" />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label>Altura</Form.Label>
+              <Form.Control type="number" placeholder="Ingrese Altura" />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label>ID Provincia</Form.Label>
+              <Form.Control type="number" placeholder="Ingrese ID provincia" />
+            </Form.Group>
+
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseModalClientes}>
+            Cancelar
+          </Button>
+          <Button variant="primary" onClick={handleCloseModalClientes}>
+            Guardar
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+
     </>
   )
 }
